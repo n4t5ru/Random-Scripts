f992213536cc3a13065e1663b43339cf2d26e3de
n4t5ru <hello@nasru.me>
2022-06-15 11:58:21 +0500

    Some updates

--- updateDesignation.py
+++ updateDesignation.py
@@ -4,11 +4,15 @@ import subprocess
 designationsdict = {}
 
 # File should be of just two columns, column one for name or IDENTITY used as AD Login. Second Column with TITLE or Designation.
-designationCSV = "" #insert file location
+importFile = "" #insert file location
 
-with open(designationCSV, "r") as inp:
+with open(importFile, "r") as inp:
     reader = csv.reader(inp)
-    designationsdict = {rows[0]:rows[1] for rows in reader}
+    importFile = {rows[0]:rows[1] for rows in reader}
 
-for name in designationsdict:
-    subprocess.call("Powershell.exe Set-ADUser -Identity "+name+" -Title '"+designationsdict[name]+"'", shell=True)
+for name in importFile:
+    #change title/designation of the User
+    subprocess.call("Powershell.exe Set-ADUser -Identity "+name+" -Title '"+importFile[name]+"'", shell=True)
+
+    #change email address of the user
+    subprocess.call("Powershell.exe Set-ADUser -Identity "+name+" -EmailAddress '"+importFile[name]+"'", shell=True)
